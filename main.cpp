497191dd1b25642b02871ee109a38fa48aae5c95
omarr45 <58887202+omarr45@users.noreply.github.com>
2023-10-23 11:26:59 +0300

    feat: add getAverage function

--- main.cpp
+++ main.cpp
@@ -13,10 +13,20 @@ int getSum(vector<int> nums)
   return sum;
 }
 
+float getAverage(vector<int> nums)
+{
+  int sum = getSum(nums);
+  return sum * 1.0 / nums.size();
+}
+
 int main()
 {
   vector<int> nums = {37, 40, 46, 38, 25, 32, 48, 11, 28, 44};
   int sum = getSum(nums);
-  cout << sum << endl;
+  cout << "Sum is: " << sum << endl;
+
+  float average = getAverage(nums);
+  cout << "Average is: " << average << endl;
+
   return 0;
 }
