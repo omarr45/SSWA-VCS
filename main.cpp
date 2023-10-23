a8ce0192cf4405b47d9fbff84980c58667877e7c
omarr45 <58887202+omarr45@users.noreply.github.com>
2023-10-23 11:29:15 +0300

    feat: add getMin function

--- main.cpp
+++ main.cpp
@@ -13,10 +13,27 @@ int getSum(vector<int> nums)
   return sum;
 }
 
+int getMin(vector<int> nums)
+{
+  int min = nums[0];
+  for (int i = 0; i < nums.size(); i++)
+  {
+    if (nums[i] < min)
+    {
+      min = nums[i];
+    }
+  }
+  return min;
+}
+
 int main()
 {
   vector<int> nums = {37, 40, 46, 38, 25, 32, 48, 11, 28, 44};
   int sum = getSum(nums);
-  cout << sum << endl;
+  cout << "Sum is: " << sum << endl;
+
+  int min = getMin(nums);
+  cout << "Min is: " << min << endl;
+
   return 0;
 }
