d3a7b601be24f9f7eeac93594c93ad0ff6223198
omarr45 <58887202+omarr45@users.noreply.github.com>
2023-10-23 11:20:04 +0300

    feat: add getSum function

--- main.cpp
+++ main.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+int getSum(vector<int> nums)
+{
+  int sum = 0;
+  for (int i = 0; i < nums.size(); i++)
+  {
+    sum += nums[i];
+  }
+  return sum;
+}
+
+int main()
+{
+  vector<int> nums = {37, 40, 46, 38, 25, 32, 48, 11, 28, 44};
+  int sum = getSum(nums);
+  cout << sum << endl;
+  return 0;
+}
